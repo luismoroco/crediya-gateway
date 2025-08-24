942367d63f6bf63776a74886f10835e9362a3fef
lmorocoramos <lmorocoramos@gmail.com>
2025-08-24 14:39:49 -0500

    init project.

---
+++ src/main/java/com/crediya/gateway/CrediyaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.crediya.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediyaGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrediyaGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/crediya/gateway/CrediyaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crediya.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrediyaGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
